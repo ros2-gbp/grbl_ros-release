c8a5f0e4643a39eda940d4216104c51d0977546e
Evan Flynn <evanflynn.msu@gmail.com>
2020-10-21 16:48:26 -0700

    Rebase from 'upstream'

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ package_name = 'grbl_ros'
 
 setuptools.setup(
     name=package_name,
-    version='0.2.1',
+    version='0.0.13',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py'))
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
     ],
     install_requires=[
         'pyserial',
@@ -26,7 +27,9 @@ setuptools.setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'grbl_node = grbl_ros.node:main'
+            'grbl_node = grbl_ros.device:main',
+            'grbl_service = grbl_ros.service:main',
+            'grbl_manager = grbl_ros.manager:main'
         ],
     },
 )
